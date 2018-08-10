5762981cce6dfdf7d32f605e441ea214eb60e8c9
Laura <laurav.zarco@gmail.com>
2018-08-10 17:49:14 +0200

    ev changes

--- src/App.js
+++ src/App.js
@@ -6,85 +6,99 @@ import Character from './Components/Character'
 import { Route, Switch } from 'react-router-dom';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      list: [],
-      inputFilterValue: ''
+    constructor(props) {
+        super(props);
+        this.state = {
+            list: [],
+            inputFilterValue: '',
+            selectValue: ''
+        }
+        this.fetchCharacters = this.fetchCharacters.bind(this);
+        this.handleFilter = this.handleFilter.bind(this);
+        this.handleSelect = this.handleSelect.bind(this)
     }
-    this.fetchCharacters = this.fetchCharacters.bind(this);
-    this.handleFilter = this.handleFilter.bind(this);
-  }
 
-  // fectch de la api
-  componentDidMount() {
-    this.fetchCharacters();
-  }
+    // fectch de la api
+    componentDidMount() {
+        this.fetchCharacters();
+    }
 
 
-  fetchCharacters() {
-    fetch('https://hp-api.herokuapp.com/api/characters')
-      .then((response) => response.json())
-      .then((json) => {
-        const listWithIds = json.map((wizard, i) => ({ ...wizard, id: '' + i })
-        )
-        this.setState({
-          list: listWithIds
-        });
-      })
-  }
-
-  //filtro
-  handleFilter(event) {
-    this.setState({
-      inputFilterValue: event.target.value
-    });
-  }
-
-  render() {
-    const filteredList = this.state.list.filter((wizard) => wizard.name.toLowerCase().includes(this.state.inputFilterValue.toLowerCase()));
-    return (
-      <div>
-        <header>
-          <h1 className="titlePage"> Harry Potter Characters </h1>
-        </header>
-        <main>
-          <Switch>
-            <Route
-              exact path='/'
-              render={() =>
-                <div className="contentPage">
-                  <Filter
-                    value={this.state.inputFilterValue}
-                    onChange={this.handleFilter} />
-                  <CharacterList
-                    className="listPage"
-                    list={filteredList}
-                  />
-                </div>}
-            />
-            <Route
-              path='/character/:id'
-              render={(props) => {
-                const id = props.match.params.id;
-                const character = this.state.list.find((wizard) => wizard.id === id)
-                return (
-                  <Character
-                    wizardPhoto={character.image}
-                    wizardName={character.name}
-                    hogwartsHouse={character.house}
-                    wizardYear={character.yearOfBirth}
-                    wizardPatronus={character.patronus}
-                    wizardIsAlive={character.alive}
-                  />
+    fetchCharacters() {
+        fetch('https://hp-api.herokuapp.com/api/characters')
+            .then((response) => response.json())
+            .then((json) => {
+                const listWithIds = json.map((wizard, i) => ({ ...wizard, id: '' + i })
                 )
-              }} />
-          </Switch>
+                this.setState({
+                    list: listWithIds
+                });
+            })
+    }
 
-        </main>
-      </div>
-    );
-  }
+    //filtro
+    handleFilter(event) {
+        this.setState({
+            inputFilterValue: event.target.value
+        });
+    }
+
+    handleSelect(event) {
+        this.setState({
+            selectValue: event.target.value
+        });
+        console.log('hola')
+    }
+
+    render() {
+        const filteredList = this.state.list
+            .filter((wizard) => wizard.name.toLowerCase().includes(this.state.inputFilterValue.toLowerCase()))
+            .filter((wizard) => wizard.ancestry.includes(this.state.selectValue));
+        return (
+            <div>
+                <header>
+                    <h1 className="titlePage"> Harry Potter Characters </h1>
+                </header>
+                <main>
+                    <Switch>
+                        <Route
+                            exact path='/'
+                            render={() =>
+                                <div className="contentPage">
+                                    <Filter
+                                        value={this.state.inputFilterValue}
+                                        onChange={this.handleFilter}
+                                        valuetwo={this.state.selectValue}
+                                        onChangetwo={this.handleSelect} />
+                                    <CharacterList
+                                        className="listPage"
+                                        list={filteredList}
+                                    />
+                                </div>}
+                        />
+                        <Route
+                            path='/character/:id'
+                            render={(props) => {
+                                const id = props.match.params.id;
+                                console.log(props.match);
+                                const character = this.state.list.find((wizard) => wizard.id === id)
+                                return (
+                                    <Character
+                                        wizardPhoto={character.image}
+                                        wizardName={character.name}
+                                        hogwartsHouse={character.house}
+                                        wizardYear={character.yearOfBirth}
+                                        wizardPatronus={character.patronus}
+                                        wizardIsAlive={character.alive}
+                                    />
+                                )
+                            }} />
+                    </Switch>
+
+                </main>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/Components/Filter.js
+++ src/Components/Filter.js
@@ -3,11 +3,18 @@ import React, { Component } from 'react';
 class Filter extends Component {
     render() {
         return (
-            <input
-                type="text"
-                value={this.props.value}
-                onChange={this.props.onChange}
-            />
+            <div>
+                <input
+                    type="text"
+                    value={this.props.value}
+                    onChange={this.props.onChange}
+                />
+                <select onChange={this.props.onChangetwo}>
+                    <option value="half-blood" >half-blood</option>
+                    <option value="pure-blood">pure-blood</option>
+                    <option value="muggleborn">muggleborn</option>
+                </select>
+            </div>
         )
     }
 }
